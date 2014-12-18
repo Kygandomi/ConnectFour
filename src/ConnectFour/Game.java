249f3665bdf1e5ef6633f493bec664eaa9055449
Katie <Katie.gandomi@gmail.com>
2014-12-18 15:34:11 -0500

    Final version of ConnectFour code for RBE4815

--- src/ConnectFour/Game.java
+++ src/ConnectFour/Game.java
@@ -1,13 +1,20 @@
 package ConnectFour;
 
+import gnu.io.SerialPortEvent;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Game {
+	boolean useSerial = false;
 	boolean gameFinished = false;
 	Board board;
 	IContestant contestant1;
 	IContestant contestant2;
 	Scanner scan;
+	Serial mySerial = new Serial();
+	//SerialPortEvent oEvent = new SerialPortEvent(mySerial, DATA_AVAILABLE, getOldValue(), getnewValue());
 	
 	Game(){
 		this.board = new Board();
@@ -19,6 +26,17 @@ public class Game {
 	 * establishes which type of game the player wishes to play
 	 */
 	public void setUp(){
+		System.out.println("Would you like to send data over serial? (yes/no)");
+		String choice1 = scan.nextLine();
+		if(choice1.equals("yes")){
+			useSerial = true;
+			mySerial.initialize();
+		} else if(choice1.equals("no")){
+			useSerial = false;
+		} else{
+			this.setUp();
+		}
+		
 		System.out.println("Enter 1 Player v. Player");
 		System.out.println("Enter 2 Player v. Computer");
 		System.out.println("Enter 3 Computer v. Computer");
@@ -32,6 +50,9 @@ public class Game {
 				break;
 			case 3:
 				initCvC();
+				break;
+			default:
+				this.setUp();
 				break;
 		}
 		
@@ -70,22 +91,52 @@ public class Game {
 	public void playGame(){
 		this.setUp();
 		board.setUpBoard();
+		
 		while(!gameFinished){
 			System.out.print("Contestant 1: ");
-			gameFinished = board.update(this.contestant1.makeMove(board));
+			Chip one = this.contestant1.makeMove(board);
+			if(useSerial){
+				byte[] b = {(byte) one.get(), '\n'};
+				mySerial.sendData(b);
+			}
+			gameFinished = board.update(one);
 			board.printBoard();
-			if (gameFinished){
+			 if(gameFinished){
 				System.out.println('+' + " is the WINNER!");
 				break;
 			}
+			 System.out.println("Loading...");
+			try {
+			    Thread.sleep(15000);                 //1000 milliseconds is one second.
+				} 
+			catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			   }
+			System.out.println("Motion complete. Resume.");
+			 
 			System.out.print("Contestant 2: ");
-			gameFinished = board.update(this.contestant2.makeMove(board));
+			Chip two = this.contestant2.makeMove(board);
+			if(useSerial){
+				byte[] b = {(byte)(two.get() + 7), '\n'};
+				mySerial.sendData(b);
+			}
+			gameFinished = board.update(two);
 			board.printBoard();
 			if (gameFinished){
 				System.out.println('x' + " is the WINNER!");
 				break;
 			}
-		}
+			
+			System.out.println("Loading...");
+			try {
+			    Thread.sleep(15000);                 //1000 milliseconds is one second.
+				} 
+			catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			   }
+			System.out.println("Motion complete. Resume.");
+		}	
+		
 	}
 
 }
